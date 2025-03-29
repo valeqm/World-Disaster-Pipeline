8049dd244616bc50084e931773a51a4a8aed21a9
valeqm <valeriaqm_24_11@hotmail.com>
2025-03-29 12:38:44 -0500

    Upload dbt

--- dbt/models/core/join_disaster.sql
+++ dbt/models/core/join_disaster.sql
@@ -12,8 +12,10 @@ fact_disaster AS (
 )
 
 SELECT 
-    d.*,
-    f.*
+    d.id_disaster,
+    d.* EXCEPT(id_disaster),
+    f.* EXCEPT(id_disaster)
 FROM dim_disaster d
 LEFT JOIN fact_disaster f
 ON d.id_disaster = f.id_disaster
+
